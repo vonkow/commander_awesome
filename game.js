99b37baf90e483a73c10d966de2c676e993baf60
Caz vonKow <skopsycats@gmail.com>
2010-05-08 19:08:57 -0400

    added sting's graphics to the preloader

--- game.js
+++ game.js
@@ -74,6 +74,7 @@ var startGame=function() {
 	.using('villans/blob','png',['l1','l2','r1','r2'])
 	.using('villans/baldo','png',['u1','u2','d1','d2','l1','l2','r1','r2'])
 	.using('villans/shades','png',['u1','u2','d1','d2','l1','l2','r1','r2'])
+	.using('villans/sting','png',['u1','u2','d1','d2','l1','l2','r1','r2'])
 	.setFPS(60)
 	.newEnt(new lagTimer()).base.display('blank',0,0,0).end()
 	.func(world2())
